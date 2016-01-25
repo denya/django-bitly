2e0eb4dcb715e636e3172f2e19b7b7a1b54e456b
Denis Moskalets <denya.msk@gmail.com>
2016-01-25 19:39:37 +0300

    Update for django 1.9

--- django_bitly/models.py
+++ django_bitly/models.py
@@ -10,7 +10,7 @@ except ImportError:
 from django.db import models
 from django.contrib.sites.models import Site
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.conf import settings
 from django.utils import simplejson as json
 
@@ -129,7 +129,7 @@ class Bittle(models.Model):
     """An object representing a Bit.ly link to a local object."""
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey('content_type', 'object_id')
     absolute_url = models.URLField()
 
     hash = models.CharField(max_length=10)
